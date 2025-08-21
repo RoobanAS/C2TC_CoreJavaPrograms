0f8ebdff718caadafbc697ac9387e357b02bd4e2
RoobanAS <133811664+RoobanAS@users.noreply.github.com>
2025-08-21 09:29:12 +0530

    Update Student.java

--- C2TC_CoreJavaPrograms/src/dayseven/overriding/covariant/Student.java
+++ C2TC_CoreJavaPrograms/src/dayseven/overriding/covariant/Student.java
@@ -1,5 +1,5 @@
 //Program to define class with Covariant Method Overriding 
-package com.tnsif.dayseven.overriding.covariant;
+package dayseven.overriding.covariant;
 
 //Student class
 class Student implements Cloneable {
