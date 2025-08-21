25191fccaf62670f2428766041d7d18ceff59fd7
RoobanAS <133811664+RoobanAS@users.noreply.github.com>
2025-08-21 09:27:45 +0530

    Update Point.java

--- C2TC_CoreJavaPrograms/src/dayseven/overloading/Point.java
+++ C2TC_CoreJavaPrograms/src/dayseven/overloading/Point.java
@@ -1,5 +1,5 @@
 //Program to demonstrate constructor overloading - Compile Time Polymorphism
-package com.tnsif.dayseven.overloading;
+package dayseven.overloading;
 
 public class Point {
 	private float x;
